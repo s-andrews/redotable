2c1c20fca628c200c4e35c19506b571cbb145ccc
s-andrews <simon.andrews@babraham.ac.uk>
2018-08-01 10:39:20 +0100

    Basic UI present.

--- uk/ac/babraham/redotable/RedotableApplication.java
+++ uk/ac/babraham/redotable/RedotableApplication.java
@@ -1,8 +1,13 @@
 package uk.ac.babraham.redotable;
 
+import java.io.File;
+
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.filechooser.FileFilter;
 
 import uk.ac.babraham.redotable.datatypes.SequenceCollection;
+import uk.ac.babraham.redotable.parsers.SequenceParser;
 import uk.ac.babraham.redotable.utilities.ProgressListener;
 
 public class RedotableApplication extends JFrame implements ProgressListener {
@@ -12,10 +17,56 @@ public class RedotableApplication extends JFrame implements ProgressListener {
 	
 	
 	public RedotableApplication () {
-		setJMenuBar(new RedotableMenu());
+		setJMenuBar(new RedotableMenu(this));
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(800, 800);
+		setLocationRelativeTo(null);
+		setVisible(true);
 		
 		
 	}
+	
+	public void openxseqs (){
+		openseqs("xseqs");
+	}
+	public void openyseqs (){
+		openseqs("yseqs");
+	}
+	
+	
+	private void openseqs(String tag) {
+		JFileChooser chooser = new JFileChooser("C:/Users/andrewss/Desktop/redotable/");
+		chooser.setMultiSelectionEnabled(false);
+		chooser.setFileFilter(new FileFilter() {
+		
+			public String getDescription() {
+				return "FastA sequence files";
+			}
+		
+			public boolean accept(File f) {
+				if (f.isDirectory() || f.getName().toLowerCase().endsWith(".fa") || f.getName().toLowerCase().endsWith(".fasta")) {
+					return true;
+				}
+				else {
+					return false;
+				}
+			}
+		
+		});
+		
+		int result = chooser.showOpenDialog(this);
+		if (result == JFileChooser.CANCEL_OPTION) return;
+
+		File file = chooser.getSelectedFile();
+
+		SequenceParser sp = new SequenceParser(file, tag);
+		sp.addListener(this);
+		
+		sp.startParsing();
+
+	}
+	
 	
 	
 	public static void main(String[] args) {
@@ -24,37 +75,30 @@ public class RedotableApplication extends JFrame implements ProgressListener {
 
 
 	@Override
-	public void progressExceptionReceived(Exception e) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void progressExceptionReceived(Exception e) {}
 
 
 	@Override
-	public void progressWarningReceived(Exception e) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void progressWarningReceived(Exception e) {}
 
 
 	@Override
-	public void progressUpdated(String message, int current, int max) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void progressUpdated(String message, int current, int max) {}
 
 
 	@Override
-	public void progressCancelled() {
-		// TODO Auto-generated method stub
-		
-	}
+	public void progressCancelled() {}
 
 
 	@Override
 	public void progressComplete(String command, Object result) {
-		// TODO Auto-generated method stub
-		
+		if (command.equals("xseqs")) {
+			collectionX = (SequenceCollection)result;
+		}
+		else if (command.equals("yseqs")) {
+			collectionY = (SequenceCollection)result;
+		}
+
 	}
 
 }
--- uk/ac/babraham/redotable/RedotableMenu.java
+++ uk/ac/babraham/redotable/RedotableMenu.java
@@ -1,7 +1,58 @@
 package uk.ac.babraham.redotable;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
-public class RedotableMenu extends JMenuBar {
+public class RedotableMenu extends JMenuBar implements ActionListener {
 
+	private RedotableApplication application;
+	
+	public RedotableMenu (RedotableApplication application) {
+		this.application = application;
+		
+		JMenu fileMenu = new JMenu("File");
+		fileMenu.setMnemonic(KeyEvent.VK_F);
+		
+		JMenuItem fileOpenX = new JMenuItem("Open X Sequences...");
+		fileOpenX.setActionCommand("openx");
+		fileOpenX.addActionListener(this);
+		fileMenu.add(fileOpenX);
+
+		JMenuItem fileOpenY = new JMenuItem("Open Y Sequences...");
+		fileOpenY.setActionCommand("openy");
+		fileOpenY.addActionListener(this);
+		fileMenu.add(fileOpenY);
+
+		JMenuItem fileExit = new JMenuItem("Exit");
+		fileExit.setActionCommand("exit");
+		fileExit.addActionListener(this);
+
+		add(fileMenu);
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+
+		if (ae.getActionCommand().equals("openx")) {
+			application.openxseqs();
+		}
+		else if (ae.getActionCommand().equals("openy")) {
+			application.openyseqs();
+		}
+		else if (ae.getActionCommand().equals("exit")) {
+			System.exit(0);
+		}
+		else {
+			throw new IllegalStateException("Unknown menu command "+ae.getActionCommand());
+		}
+		
+	}
+	
+	
 }
