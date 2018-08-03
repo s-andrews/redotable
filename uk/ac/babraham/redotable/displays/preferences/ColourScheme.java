3e9a1ba9323d28f8c9901353f5df5c877abd386e
s-andrews <simon.andrews@babraham.ac.uk>
2018-08-03 11:07:11 +0100

    Added a colour scheme class

---
+++ uk/ac/babraham/redotable/displays/preferences/ColourScheme.java
@@ -0,0 +1,13 @@
+package uk.ac.babraham.redotable.displays.preferences;
+
+import java.awt.Color;
+
+public class ColourScheme {
+
+	public static Color FORWARD = Color.RED;
+	public static Color REVERSE = Color.BLUE;
+	public static Color SEQUENCE_EDGE = Color.LIGHT_GRAY;
+	public static Color SINGLE_HIGHLIGHT = Color.YELLOW;
+	public static Color DOUBLE_HIGHLIGHT = Color.ORANGE;
+	
+}
