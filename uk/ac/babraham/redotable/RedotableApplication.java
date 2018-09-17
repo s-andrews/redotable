a275624b1ed686095ca179adf54ca41b61143599
s-andrews <simon.andrews@babraham.ac.uk>
2018-09-17 11:27:42 +0100

    Bump version to 1.0 ready for release.

--- uk/ac/babraham/redotable/RedotableApplication.java
+++ uk/ac/babraham/redotable/RedotableApplication.java
@@ -52,7 +52,7 @@ import uk.ac.babraham.redotable.writers.SequenceWriterPreferences;
 
 public class RedotableApplication extends JFrame implements ProgressListener, ChangeListener {
 
-	public static final String VERSION = "0.2.devel";
+	public static final String VERSION = "1.0";
 
 
 	private RedotabledData data;
